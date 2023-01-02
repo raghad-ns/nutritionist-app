0e120a5e70dd59dffcdb53fe4984cc999e851a7f
raghad__ns <raghadnadersalem20022017@gmail.com>
2023-01-02 22:15:45 +0200

    setting up food table page

---
+++ src/components/food-table/food-row/food-row.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './food-row.css'
+
+const FoodRow = () => {
+    return (
+        <tr>
+            <td>food</td>
+            <td>image</td>
+            <td>amount</td>
+            <td>calories</td>
+            <td>action</td>
+        </tr>
+    )
+}
+
+export default FoodRow
--- src/pages/food-table/food-table.page.jsx
+++ src/pages/food-table/food-table.page.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import FoodRow from '../../components/food-table/food-row/food-row.component'
 
 const FoodTable = () => {
     return (
-        <div>FoodTable</div>
+        <div>
+            <table>
+                <tr>
+                    <th>food</th>
+                    <th>image</th>
+                    <th>amount</th>
+                    <th>calories</th>
+                    <th>action</th>
+                </tr>
+                <FoodRow/>
+            </table>
+        </div>
     )
 }
 
