708fc30c968dba9e98ad42723833a33ad80aab08
raghad__ns <raghadnadersalem20022017@gmail.com>
2023-01-02 16:14:48 +0200

    seting up home page

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import './home.css'
 
 const Home = () => {
+    const navigate = useNavigate();
     return (
-        <div>Home</div>
+        <div className='home'>
+            <div className="intro">
+                <button className="homePageButtons"
+                    onClick={() => navigate('/viewPrograms')}>
+                    view existing programs
+                </button>
+                <button className="homePageButtons"
+                    onClick={() => navigate("/add")}>
+                    New diet program
+                </button>
+                <button className="homePageButtons"
+                    onClick={() => navigate("/foodTable")}>
+                    manage food table
+                </button>
+            </div>
+        </div>
     )
 }
 
