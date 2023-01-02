a51e85fb55f55e871d5217b47e75c3da88b075d6
raghad__ns <raghadnadersalem20022017@gmail.com>
2023-01-02 18:51:09 +0200

    display page name in the header

--- src/components/core/header/header.component.jsx
+++ src/components/core/header/header.component.jsx
@@ -5,16 +5,20 @@ import { House } from 'phosphor-react'
 import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
-    const navigate = useNavigate() ;
+    const navigate = useNavigate();
     return (
         <div className='header'>
             <div className='title'>
                 <img src={logo} alt="logo" />
-                <span>Nutrition Clinic Manager</span>
+                <span>Nutrition Clinic Manager </span>
+                {(window.location.pathname === "/add") && <span className='pageName'>&nbsp; - new diet program</span>}
+                {(window.location.pathname === "/viewPrograms") && <span className='pageName'>&nbsp; - new diet program</span>}
+                {(window.location.pathname === "/foodTable") && <span className='pageName'>&nbsp; - new diet program</span>}
             </div>
+
             {
                 window.location.pathname !== '/' &&
-                <button onClick={() => navigate('/')}> <House size={45} color='white' weight='fill' /> </button>
+                <button onClick={() => navigate('/')} > <House size={45} color='white' weight='fill' className="homeButton" /> </button>
             }
         </div>
     )
