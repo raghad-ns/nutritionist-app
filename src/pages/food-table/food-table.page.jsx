84bbb40e83b4ab7a47cd98b40c2ccd11dc0e8d4f
raghad__ns <raghadnadersalem20022017@gmail.com>
2023-01-04 18:41:13 +0200

    the general style of (Manage food table) page

--- src/pages/food-table/food-table.page.jsx
+++ src/pages/food-table/food-table.page.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
+import { Plus } from 'phosphor-react'
+import './food-table.css'
 import FoodRow from '../../components/food-table/food-row/food-row.component'
 
 const FoodTable = () => {
+    
     return (
-        <div>
-            <table>
-                <tr>
-                    <th>food</th>
-                    <th>image</th>
-                    <th>amount</th>
-                    <th>calories</th>
-                    <th>action</th>
-                </tr>
-                <FoodRow/>
+        <div className='foodTable'>
+            <div className='innerButtonWrapper'>
+                <button className='innerButton'>Add <Plus size={18} weight='bold' /></button>
+            </div>
+            <table cellSpacing={15}>
+                <thead>
+                    <tr>
+                        <th>food</th>
+                        <th>image</th>
+                        <th>amount</th>
+                        <th>calories</th>
+                        <th>action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <FoodRow />
+                    <FoodRow />
+                    <FoodRow />
+                    <FoodRow />
+                </tbody>
             </table>
         </div>
     )
