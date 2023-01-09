be7278de66ce79e4745cf4f6b231aac9bcf8a633
raghad__ns <raghadnadersalem20022017@gmail.com>
2023-01-09 18:06:21 +0200

    css edits

--- src/components/add/tabs/tabs.component.jsx
+++ src/components/add/tabs/tabs.component.jsx
@@ -5,10 +5,8 @@ import Tab from '../../common/tab/tab.component'
 
 const Tabs = () => {
     return (
-        <div id="container">
-            <div className="tabs">
-                {DAYS.map((day, index) => <Tab day={day} key={index} />)}
-            </div>
+        <div className="tabs">
+            {DAYS.map((day, index) => <Tab day={day} key={index} />)}
         </div>
     )
 }
