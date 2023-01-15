3ac288fd3d947e7fd6748cf2660e611bb6ead39d
raghad__ns <raghadnadersalem20022017@gmail.com>
2023-01-15 18:38:28 +0200

    enhancing specific program page

--- src/pages/specific-program/specific-program.page.jsx
+++ src/pages/specific-program/specific-program.page.jsx
@@ -1,15 +1,33 @@
 import React, { useContext } from 'react'
+import './specific-program.css'
 import { useParams } from 'react-router-dom'
 import { DietProgramsContext } from '../../components/providers/diet-programs.provider';
-import ProgramCard from '../../components/view-programs/card/card.component';
+import MealCard from '../../components/add/meal-card/meal-card.component';
 
 const SpecificProgram = () => {
-    const param = useParams();
-    const dietProgramsContext = useContext(DietProgramsContext);
-    const program = dietProgramsContext.dietPrograms.find(diet => diet.id === Number(param.id));
+  const param = useParams();
+  const dietProgramsContext = useContext(DietProgramsContext);
+  const program = dietProgramsContext.dietPrograms.find(diet => diet.id === Number(param.id));
+  const days = ['saturday', 'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
   return (
     <div className='specificProgram'>
-        <ProgramCard program = {program} />
+      <h1>{program.patientInfo.name}'s program : </h1>
+      {
+        program.mealsPerDay.map((mealsForThisDay , index) =>
+          <div className="mealsForThisDay" key={index}>
+            <div className="day">
+              {days[index]}
+            </div>
+            <div className="meals">
+              {
+                mealsForThisDay.length 
+                ?mealsForThisDay.map((meal , index) => <MealCard {...meal} key = {index}/>)
+                : <span> no meals for this day</span>
+              }
+            </div>
+          </div>
+        )
+      }
     </div>
   )
 }
