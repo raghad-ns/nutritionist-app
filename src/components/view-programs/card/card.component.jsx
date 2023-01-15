186dbe901b6c9e4ecaa87834e72ba0a186ecfe0b
raghad__ns <raghadnadersalem20022017@gmail.com>
2023-01-15 23:45:16 +0200

    trying to generate pdf file

--- src/components/view-programs/card/card.component.jsx
+++ src/components/view-programs/card/card.component.jsx
@@ -1,8 +1,11 @@
+// import jsPDF from 'jspdf'
 import { FilePdf, TrashSimple } from 'phosphor-react'
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { DietProgramsContext } from '../../providers/diet-programs.provider'
+// import ReactPDF from '@react-pdf/renderer';
 import './card.css'
+// import image from '../../../assets/not-found.jpg'
 
 /**
  * 
@@ -35,13 +38,35 @@ const ProgramCard = (props) => {
     meals.forEach(meal => calories += Number(meal.calories))
   })
 
+  const generatePDF = () => {
+    // let doc = new jsPDF('portrait' , 'px' , 'a4' , 'false');
+    // doc.addImage(image , 'JPG' , 65 , 20 , 500 , 400) ; 
+    // doc.save('test');
+
+    // ReactPDF.render(<MyDocument />, `${__dirname}/example.pdf`);
+
+    // // using Java Script method to get PDF file
+    // fetch('Sample.pdf').then(response => {
+    //   response.blob().then(blob => {
+    //     // Creating new object of PDF file
+    //     const fileURL = window.URL.createObjectURL(blob);
+    //     // Setting various property values
+    //     let alink = document.createElement('a');
+    //     alink.href = fileURL;
+    //     alink.download = 'Sample.pdf';
+    //     alink.click();
+    //   })
+    // })
+
+  }
+
   return (
     <div className='programCard'>
       <div className='details'>
         <span>Name : {props.program.patientInfo.name} </span>
         <span>Weekly calories : {calories} </span>
         {/* <span>View details </span> */}
-        <Link to = {`/dietProgram/${props.program.id}`}>View details</Link>
+        <Link to={`/dietProgram/${props.program.id}`}>View details</Link>
       </div>
       <div className='actionButtons'>
         <button
@@ -49,7 +74,7 @@ const ProgramCard = (props) => {
         >
           <TrashSimple size={24} color='red' />
         </button>
-        <button><FilePdf size={26} /></button>
+        <button onClick={generatePDF}><FilePdf size={26} /></button>
       </div>
     </div>
   )
