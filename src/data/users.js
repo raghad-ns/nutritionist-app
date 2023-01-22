e74f5c5d3143d2467f754b5bf9f149669c290726
raghad__ns <raghadnadersalem20022017@gmail.com>
2023-01-22 19:55:51 +0200

    edit names for users

--- src/data/users.js
+++ src/data/users.js
@@ -1,7 +1,7 @@
 const USERS = [
     { name: 'raghad' , email: 'raghad@gmail.com', password: '12345' },
-    { name: 'raghad' , email: 'dala@gmail.com', password: '12345' },
-    { name: 'raghad' , email: 'sajeda@gmail.com', password: '12345' },
-    { name: 'raghad' , email: 'yazan@gmail.com', password: '12345' }
+    { name: 'dala' , email: 'dala@gmail.com', password: '12345' },
+    { name: 'sajeda' , email: 'sajeda@gmail.com', password: '12345' },
+    { name: 'yazan' , email: 'yazan@gmail.com', password: '12345' }
 ];
 export { USERS };
